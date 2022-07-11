1336a27c65c4d6abb3b32390e06070d05f8a53d9
11akanksha <BT@1905373@kiit.ac.in>
2022-07-11 16:52:44 +0530

    NewsMonkey Third Commit

--- src/components/News.js
+++ src/components/News.js
@@ -1,107 +1,99 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
-export class News extends Component {
-    static defaultProps = {
-        country: 'in',
-        pageSize: 6,
-        category: 'general'
-    }
+const News = (props) => {
+    const [articles, setArticles] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [page, setPage] = useState(1)
+    const [totalResults, setTotalResults] = useState(0)
+    // document.title = `${capitalize(props.category)} - NewsMonkey`;
 
-    static propTypes = {
-        country: PropTypes.string,
-        pageSize: PropTypes.number,
-        category: PropTypes.string,
-        apiKey: PropTypes.string
-    }
-
-    capitalize = (string) => {
+    const capitalize = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            articles: [],
-            loading: true,
-            page: 1,
-            totalResults: 0
-        }
-        document.title = `${this.capitalize(this.props.category)} - NewsMonkey`;
-    };
-
-    async updateNews() {
-        this.props.setProgress(0);
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const updateNews = async () => {
+        props.setProgress(0);
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
         // const url = "https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=ef7c7adae6ff4c2da83639a2af614a21 ";
-        this.setState({ loading: true });
+        setLoading(true);
         let data = await fetch(url);
-        this.props.setProgress(30);
+        props.setProgress(30);
         let parsedData = await data.json();
-        this.props.setProgress(60);
-        this.setState({
-            articles: parsedData.articles,
-            totalResults: parsedData.totalResults,
-            loading: false
-        });
-        this.props.setProgress(100);
+        props.setProgress(60);
+        setArticles(parsedData.articles);
+        setLoading(false);
+        setTotalResults(parsedData.totalResults);
+        props.setProgress(100);
     }
 
-    async componentDidMount() {
-        this.updateNews();
+    useEffect(() => {
+        updateNews();
+    }, [])
+
+    const handlePrevClick = async () => {
+        setPage(page - 1);
+        updateNews();
     }
 
-    handlePrevClick = async () => {
-        await this.setState({ page: this.state.page - 1 });
-        this.updateNews();
+    const handleNextClick = async () => {
+        setPage(page + 1);
+        updateNews();
     }
 
-    handleNextClick = async () => {
-        await this.setState({ page: this.state.page + 1 });
-        this.updateNews();
-    }
-
-    fetchMoreData = async () => {
-        this.setState({ page: this.state.page + 1 });
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const fetchMoreData = async () => {
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`;
+        setPage(page + 1);
+        /*// Another Bug is that the first 6 news gets duplicated!!Tried, but couldn't fix it!!
+        //////// Hence the BUG is FIXED!!***********////////
         // const url = "https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=ef7c7adae6ff4c2da83639a2af614a21";
         let data = await fetch(url);
         let parsedData = await data.json();
-        this.setState({
-            articles: this.state.articles.concat(parsedData.articles),
-            totalResults: parsedData.totalResults,
-        });
+        setArticles(articles.concat(parsedData.articles));
+        setTotalResults(parsedData.totalResults);
     }
     // Another Bug is that the first 6 news gets duplicated!!Tried, but couldn't fix it!!
-    render() {
-        return (
-            <>
-                <h2 className='text-center' style={{ margin: '35px 0px' }}>NewsMonkey - Top {this.capitalize(this.props.category)} Headlines</h2>
-                {this.state.loading && <Spinner />}
-                <InfiniteScroll
-                    dataLength={this.state.articles.length}
-                    next={this.fetchMoreData}
-                    hasMore={this.state.articles.length !== this.state.totalResults}
-                    loader={<Spinner />}
-                >
-                    <div className="container">
-                        <div className="row">
-                            {!this.state.loading && this.state.articles.map((element) => {
-                                return <div className="col-md-4" key={element.url}>
-                                    <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""}
-                                        imageUrl={element.urlToImage} newsUrl={element.url} author={element.author ? element.author : "Unknown"} date={element.publishedAt} source={element.source.name} />
-                                    {/* <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} /> */}
-                                </div>
-                            })}
-                        </div>
+    return (
+        <>
+            <h2 className='text-center' style={{ margin: '35px 0px', marginTop: '90px' }}>NewsMonkey - Top {capitalize(props.category)} Headlines</h2>
+            {loading && <Spinner />}
+            <InfiniteScroll
+                dataLength={articles.length}
+                next={fetchMoreData}
+                hasMore={articles.length !== totalResults}
+                loader={<Spinner />}
+            >
+                <div className="container">
+                    <div className="row">
+                        {articles.map((element) => {
+                            return <div className="col-md-4" key={element.url}>
+                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""}
+                                    imageUrl={element.urlToImage} newsUrl={element.url} author={element.author ? element.author : "Unknown"} date={element.publishedAt} source={element.source.name} />
+                                {/* <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} /> */}
+                            </div>
+                        })}
                     </div>
-                </InfiniteScroll>
-            </>
-        )
-    }
+                </div>
+            </InfiniteScroll>
+        </>
+    )
 }
+
+News.defaultProps = {
+    country: 'in',
+    pageSize: 6,
+    category: 'general'
+}
+
+News.propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+    apiKey: PropTypes.string
+}
+
 
 export default News
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-    render() {
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
-        return (
-            <div className='my-3'>
-                <div className="card">
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'flex-end',
-                        position: 'absolute',
-                        right: '0'
-                    }}>
-                        <span className="badge rounded-pill bg-info">{source}</span>
-                    </div>
-                    <img src={imageUrl ? imageUrl : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLEMiDWtfT3tgMfKhM37oJg9vthWl3dkmI8Q&usqp=CAU"} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        {/* <h5 className="card-title">{title}...</h5>
+const NewsItem = (props) => {
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+        <div className='my-3'>
+            <div className="card">
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    position: 'absolute',
+                    right: '0'
+                }}>
+                    <span className="badge rounded-pill bg-info">{source}</span>
+                </div>
+                <img src={imageUrl ? imageUrl : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLEMiDWtfT3tgMfKhM37oJg9vthWl3dkmI8Q&usqp=CAU"} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    {/* <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p> */}
-                        <h5 className="card-title">
-                            {title}
-                        </h5>
-                        <p className="card-text">{description}</p>
-                        <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toGMTString()} </small></p>
-                        <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read More</a>
-                    </div>
+                    <h5 className="card-title">
+                        {title}
+                    </h5>
+                    <p className="card-text">{description}</p>
+                    <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toGMTString()} </small></p>
+                    <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read More</a>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default NewsItem
